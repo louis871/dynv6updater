1d232561ff89c80e6d1440a2ed691643232abb96
Luigi Piccinni <luipicc@tiscali.it>
2024-08-22 02:22:49 +0200

    Log messages added

--- dynv6updater.net/UpdaterService.cs
+++ dynv6updater.net/UpdaterService.cs
@@ -30,22 +30,25 @@ namespace dynv6updater.net
         private int _msControlIPv4 = 10 * 60 * 1000;
         private string _apiKeyIPv4 = "";
         private string _zoneToUpdate = "";
+        private string _logFilename = "";
 
-        private const string URL_DYNV6_API_CALL = "https://dynv6.com/api/update?zone={0}&token={1}&ipv4={2}";
-
-        public int MsControlIPv4 { get => _msControlIPv4; set => _msControlIPv4 = value; }
-        public string ApiKeyIPv4 { get => _apiKeyIPv4; set => _apiKeyIPv4 = value; }
-        public string ZoneToUpdate { get => _zoneToUpdate; set => _zoneToUpdate = value; }
+        private const string URL_DYNV6_API_CALL_FORMAT = "https://dynv6.com/api/update?zone={0}&token={1}&ipv4={2}";
+        private const string LOG_FILENAME_FORMAT = "{0}\\dynv6updater.net.log";
+        private const string LOG_MESSAGE_FORMAT = "#{0} <{1} {2}> {3}\n";
 
         protected override void OnStart(string[] args)
         {
+            _logFilename = string.Format(LOG_FILENAME_FORMAT, Path.GetDirectoryName(Application.ExecutablePath));
+            AppendLog("Service started", "I");
+
             try
             {
                 _configurationFile = ConfigurationManager.OpenExeConfiguration(Application.ExecutablePath);
             }
-            catch
+            catch(Exception e)
             {
-                throw new Exception("ERROR WHILE LOADING CONFIGURATION DATA");
+                AppendLog("Error while loading configuration data: " + getFullNestedErrorMessage(e), "E");
+                return;
             }
 
             ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
@@ -53,7 +56,7 @@ namespace dynv6updater.net
 
             _timerCheckIPv4 = new System.Timers.Timer();
             _timerCheckIPv4.Elapsed += new ElapsedEventHandler(CheckIPv4_Elapsed);
-            _timerCheckIPv4.Interval = 20 * 1000;
+            _timerCheckIPv4.Interval = 1000;
             _timerCheckIPv4.AutoReset = false;
             _timerCheckIPv4.Start();
         }
@@ -61,9 +64,10 @@ namespace dynv6updater.net
         protected override void OnStop()
         {
             _timerCheckIPv4?.Stop();
+            AppendLog("Service stopped", "I");
         }
 
-        private void CheckIPv4_Elapsed(object sender, ElapsedEventArgs e)
+        private void CheckIPv4_Elapsed(object sender, ElapsedEventArgs eArgs)
         {
             _timerCheckIPv4.Stop();
 
@@ -72,9 +76,18 @@ namespace dynv6updater.net
             try
             {
                 int mins = int.Parse(_configurationFile.AppSettings.Settings["MinutesBetweenControls"].Value);
-                _msControlIPv4 = mins * 60 * 1000;
+                int newMsControlIPv4 = mins * 60 * 1000;
+                if (newMsControlIPv4 != _msControlIPv4)
+                {
+                    _msControlIPv4 = newMsControlIPv4;
+                    AppendLog("MinutesBetweenControls set to " + mins, "I");
+                }
             }
-            catch { }
+            catch(Exception e) 
+            {
+                AppendLog("Malformed minutes configuration data: " + getFullNestedErrorMessage(e), "E");
+                AppendLog("MinutesBetweenControls set to " + (_msControlIPv4 / 60000.00), "I");
+            }
             _timerCheckIPv4.Interval = _msControlIPv4;
             _timerCheckIPv4.Start();
         }
@@ -85,7 +98,11 @@ namespace dynv6updater.net
             {
                 return _webClient.DownloadString("http://ifconfig.me").Replace("\n", "");
             }
-            catch { return string.Empty; }
+            catch(Exception e)
+            {
+                AppendLog("Actual Public IPv4 <null>: " + getFullNestedErrorMessage(e), "E");
+                return string.Empty; 
+            }
         }
 
         private void tryUpdateIPv4(string actualIPv4)
@@ -97,20 +114,55 @@ namespace dynv6updater.net
                 _apiKeyIPv4 = _configurationFile.AppSettings.Settings["ApiKeyIPv4"].Value.ToString();
                 _zoneToUpdate = _configurationFile.AppSettings.Settings["ZoneToUpdate"].Value.ToString();
             }
-            catch { return; }
-            if (string.IsNullOrEmpty(_zoneToUpdate)) return;
-            if (string.IsNullOrEmpty(_apiKeyIPv4)) return;
+            catch(Exception e)
+            {
+                AppendLog("Malformed configuration data: " + getFullNestedErrorMessage(e), "E");
+                return; 
+            }
+            if (string.IsNullOrEmpty(_zoneToUpdate))
+            {
+                AppendLog("Missing Zone configuration", "I");
+                return;
+            }
+            if (string.IsNullOrEmpty(_apiKeyIPv4))
+            {
+                AppendLog("Missing API Key configuration", "I");
+                return;
+            }
 
             try
             {
-                string url = string.Format(URL_DYNV6_API_CALL, _zoneToUpdate, _apiKeyIPv4, actualIPv4);
+                string url = string.Format(URL_DYNV6_API_CALL_FORMAT, _zoneToUpdate, _apiKeyIPv4, actualIPv4);
                 var stream = _webClient.OpenRead(url);
                 using (StreamReader sr = new StreamReader(stream))
                 {
-                    var page = sr.ReadToEnd();
+                    string output = sr.ReadToEnd();
+                    if( !output.ToLower().Contains("unchanged") )
+                        AppendLog("Zone " + _zoneToUpdate + ": ("+ actualIPv4 +")" + output, "I");
                 }
             }
-            catch { }
+            catch(Exception e)
+            {
+                AppendLog("Bad call to dynv6.com API: " + getFullNestedErrorMessage(e), "E");
+                return;
+            }
+        }
+
+        private void AppendLog(string message, string logType)
+        {
+            File.AppendAllText(_logFilename, string.Format(LOG_MESSAGE_FORMAT, logType, DateTime.Now.ToShortDateString(), DateTime.Now.ToLongTimeString(), message));
+        }
+
+        private static string getFullNestedErrorMessage(Exception e)
+        {
+            string errorMessage = e.Message;
+            while (e.InnerException != null)
+            {
+                errorMessage += " <- " + e.InnerException.Message;
+                e = e.InnerException;
+            }
+
+            return errorMessage;
         }
     }
 }
